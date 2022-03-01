c929c19b349617c88e4dcad17182780858e3bdb3
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2022-03-01 09:43:25 +0100

    testet simple fetch api request

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,27 @@
 </template>
 
 <script>
+/*   let params = "query=Zürich"
+  const Http = new XMLHttpRequest();
+  Http.responseType = "json";
+  const url='http://transport.opendata.ch/v1/locations';
+  Http.open("GET", url+"?"+params, true);
+  Http.send();
+
+  Http.onreadystatechange=(e)=>{
+    let result = Http.response
+    if(result == null)
+      return;
+    console.log(result.stations[0].name);
+    console.log("------------------------------");
+  } */
+
+  let params = "query=Zürich"
+  fetch("http://transport.opendata.ch/v1/locations?" + params)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
 export default {
   name: "App",
 
