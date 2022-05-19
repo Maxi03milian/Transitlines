4576bc7eae8e7ae51fb92966bdc570341ff65d36
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2022-05-19 09:21:39 +0200

    added connection inputs
    NOT WORKING
    inputs dont refresh respective variable for request

--- src/views/Connections.vue
+++ src/views/Connections.vue
@@ -3,20 +3,19 @@
     <Header />
     <div class="input">
       <v-autocomplete
-        v-model="state.searchInput"
+        v-model="input1"
+        :items="fromStations.stations"
+        dense
         label="From"
-        placeholder="Enter Location"
-        prepend-icon="mdi-map-marker"
-        clearable
       ></v-autocomplete>
-      <v-text-field
-        v-model="state.searchInput"
+      <v-autocomplete
+        v-model="this.input2"
+        :items="this.toStations.stations"
+        @change="toOptions(this.input2)"
+        dense
         label="To"
-        placeholder="Enter Location"
-        prepend-icon="mdi-map-marker"
-        clearable
-      ></v-text-field>
-      <v-btn class="myButton" elevation="2" @click="search(state.searchInput)"
+      ></v-autocomplete>
+      <v-btn class="myButton" elevation="2" @click="fromOptions(this.input1)"
         >Go!</v-btn
       >
     </div>
@@ -28,6 +27,14 @@ import Header from "../components/Header";
 
 export default {
   name: "Stationboard",
+  data() {
+    return {
+      input1: "",
+      input2: "",
+      fromStations: [],
+      toStations: [],
+    };
+  },
 
   components: {
     Header,
@@ -35,6 +42,11 @@ export default {
   computed: {
     state() {
       return this.$store.state;
+    },
+  },
+  watch: {
+    input1() {
+     this.formOptions(this.input1)
     },
   },
   methods: {
@@ -45,6 +57,24 @@ export default {
         .then((data) => {
           console.log(data);
           this.$store.commit("updateStations", data);
+        });
+    },
+    fromOptions(text) {
+      let params = "query=" + text;
+      fetch("http://transport.opendata.ch/v1/locations?" + params)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.fromStations = data;
+        });
+    },
+    toOptions(text) {
+      let params = "query=" + text;
+      fetch("http://transport.opendata.ch/v1/locations?" + params)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.toStations = data;
         });
     },
   },
