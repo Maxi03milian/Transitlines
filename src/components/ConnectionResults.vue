a8e828bbf065735d2902687708e91fbfc115a733
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2022-05-25 10:43:37 +0200

    formatted content on connection header

--- src/components/ConnectionResults.vue
+++ src/components/ConnectionResults.vue
@@ -9,14 +9,28 @@
         <v-expansion-panel-header>
           <div class="headerContent">
             <div class="nextConnection">
-               {{connection.products[0]}} Richtung {{connection.sections[0].journey.to}}
+              {{ connection.products[0] }} Richtung
+              {{ connection.sections[0].journey.to }}
             </div>
             <div class="connectionTimeline">
-              {{getProperTime(connection.from.departure)}} -------------------------------------------------{{getProperTime(connection.to.arrival)}}
+              <span class="alignCenter">{{
+                getProperTime(connection.from.departure)
+              }}</span>
+              <span class="alignCenter">
+                <v-divider></v-divider>
+                <div class="timelineDots">
+                  <v-icon small color="blue-grey darken-2" v-for="index in connection.transfers" :key="index">
+                    mdi-call-split
+                  </v-icon>
+                </div>
+              </span>
+              <span class="alignCenter alignRight">{{
+                getProperTime(connection.to.arrival)
+              }}</span>
             </div>
             <div class="connectionLength">
-              Platform <b>{{connection.from.platform}}</b> ---------------------------------{{getProperLength(connection.duration)}}
-
+              <span>Platform {{ connection.from.platform }}</span>
+              <span>{{getProperLength(connection.duration)}}</span>
             </div>
           </div>
         </v-expansion-panel-header>
@@ -32,13 +46,12 @@
 </template>
 
 <script>
-
 import FlexibleIcon from "../components/FlexibleIcon";
 export default {
   name: "ConnectionResults",
 
   components: {
-    FlexibleIcon
+    FlexibleIcon,
   },
   props: {
     connections: Object,
@@ -55,22 +68,54 @@ export default {
     getProperLength(time) {
       let hours = time.substr(3, 2);
       let minutes = time.substr(6, 2);
-
       let output = "";
-      if(hours != "00"){
-        output += hours + " h and"
+      if (hours != "00") {
+        var number  = hours.replace(/^0+/, ''); 
+        output += number + "h and ";
       }
-      output += minutes + " min"
-      return output
+      if(minutes != "00"){
+        var number  = minutes.replace(/^0+/, ''); 
+        output += number + " min";
+        return output;
+      }else{
+        let newoutput = output.replace(" and ", "");
+        return newoutput;
+      }
     },
   },
 };
 </script>
 
 <style>
-
-.headerContent{
+.headerContent {
   display: flex;
   flex-direction: column;
+  row-gap: 1rem;
+}
+
+.alignCenter {
+  display: flex;
+  align-items: center;
+}
+.alignRight {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.connectionTimeline {
+  display: grid;
+  grid-template-columns: 15% 70% 15%;
+}
+
+.timelineDots{
+  position: absolute;
+  width: 204px;
+  display: flex;
+  justify-content: space-around;
+}
+
+.connectionLength{
+  display: flex;
+  justify-content: space-between;
 }
 </style>
