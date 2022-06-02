5ec1e1c10dd3ad0ba3d6d1d7d260247bfd1c283a
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2022-06-02 07:57:05 +0200

    removed commented code

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,6 @@
 </template>
 
 <script>
-/*   let params = "query=Zürich"
-  const Http = new XMLHttpRequest();
-  Http.responseType = "json";
-  const url='http://transport.opendata.ch/v1/locations';
-  Http.open("GET", url+"?"+params, true);
-  Http.send();
-
-  Http.onreadystatechange=(e)=>{
-    let result = Http.response
-    if(result == null)
-      return;
-    console.log(result.stations[0].name);
-    console.log("------------------------------");
-  } */
-
 
 import Header from "@/components/Header.vue"
 
