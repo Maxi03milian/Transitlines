df99d1b0fe79f33ee5cb645748b391c7bbaf34bf
Maxi03milian <46729527+Maxi03milian@users.noreply.github.com>
2022-07-22 16:20:40 +0200

    adde navbar links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,187 +1,71 @@
 <template>
-  <div class="hello">
-   <section class="navigation">
-  <div class="nav-container">
-    <div class="brand">
-      <a href="/"><img src="../assets/logo.png" alt="Logo"></a>
-    </div>
-    <nav>
-      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
-      <ul class="nav-list">
-        <li>
-          <router-link to="/">Connections</router-link>
-        </li>
-        <li>
-          <router-link to="/calculator">Route</router-link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-</section>
+  <div class="app">
+    <v-app-bar color="#262626 accent-4" dark>
+      <div class="brand">
+        <a href="/"><img src="../assets/logo.png" alt="Logo" /></a>
+      </div>
+      <v-toolbar-title>Transitlines</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div class="nav-mobile">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </div>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      right
+      width="300"
+      style="position: fixed; top: 0; right: 0; overflow-y: scroll"
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="#262626 text--accent-4"
+        >
+          <v-list-item href="/">
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item href="/connections">
+            <v-list-item-title>Connections</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item href="/stationBoard">
+            <v-list-item-title>Stationboard</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-img{
+img {
   height: 60px;
   padding-top: 5px;
 }
-
-.navigation {
-	 height: 70px;
-	 background: #262626;
+.app {
+  margin-bottom: 1rem;
+  width: 100%;
+  overflow: hidden !important;
 }
- .brand {
-	 position: absolute;
-	 padding-left: 20px;
-	 float: left;
-	 line-height: 70px;
-	 text-transform: uppercase;
-	 font-size: 1.4em;
-}
- .brand a, .brand a:visited {
-	 color: #fff;
-	 text-decoration: none;
-}
- .nav-container {
-	 max-width: 1000px;
-	 margin: 0 auto;
-}
- nav {
-	 float: right;
-}
- nav ul {
-	 list-style: none;
-	 margin: 0;
-	 padding: 0;
-}
- nav ul li {
-	 float: left;
-	 position: relative;
-}
-
- nav ul li a, nav ul li a:visited {
-	 display: block;
-	 padding: 0 20px;
-	 line-height: 70px;
-	 background: #262626;
-	 color: #fff;
-	 text-decoration: none;
-}
- nav ul li a, nav ul li a {
-	 transition: all 0.5s 0s linear;
-}
- nav ul li a:hover, nav ul li a:visited:hover {
-	 background: #2581dc;
-	 color: #fff;
-}
- nav ul li a:not(:only-child):after, nav ul li a:visited:not(:only-child):after {
-	 padding-left: 4px;
-	 content: ' ▾';
-}
- nav ul li ul li {
-	 min-width: 190px;
-}
- nav ul li ul li a {
-	 padding: 15px;
-	 line-height: 20px;
-}
- .nav-dropdown {
-	 position: absolute;
-	 display: none;
-	 z-index: 1;
-	 box-shadow: 0 3px 12px rgba(0, 0, 0, 0.15);
-}
-/* Mobile navigation */
- .nav-mobile {
-	 display: none;
-	 position: absolute;
-	 top: 0;
-	 right: 0;
-	 background: #262626;
-	 height: 70px;
-	 width: 70px;
-}
- @media only screen and (max-width: 798px) {
-	 .nav-mobile {
-		 display: block;
-	}
-	 nav {
-		 width: 100%;
-		 padding: 70px 0 15px;
-	}
-	 nav ul {
-		 display: none;
-	}
-	 nav ul li {
-		 float: none;
-	}
-	 nav ul li a {
-		 padding: 15px;
-		 line-height: 20px;
-	}
-	 nav ul li ul li a {
-		 padding-left: 30px;
-	}
-	 .nav-dropdown {
-		 position: static;
-	}
-}
- @media screen and (min-width: 799px) {
-	 .nav-list {
-		 display: block !important;
-	}
-}
- #nav-toggle {
-	 position: absolute;
-	 left: 18px;
-	 top: 22px;
-	 cursor: pointer;
-	 padding: 10px 35px 16px 0px;
-}
- #nav-toggle span, #nav-toggle span:before, #nav-toggle span:after {
-	 cursor: pointer;
-	 border-radius: 1px;
-	 height: 5px;
-	 width: 35px;
-	 background: #fff;
-	 position: absolute;
-	 display: block;
-	 content: '';
-	 transition: all 300ms ease-in-out;
-}
- #nav-toggle span:before {
-	 top: -10px;
-}
- #nav-toggle span:after {
-	 bottom: -10px;
-}
- #nav-toggle.active span {
-	 background-color: transparent;
-}
- #nav-toggle.active span:before, #nav-toggle.active span:after {
-	 top: 0;
-}
- #nav-toggle.active span:before {
-	 transform: rotate(45deg);
-}
- #nav-toggle.active span:after {
-	 transform: rotate(-45deg);
-}
- article {
-	 max-width: 1000px;
-	 margin: 0 auto;
-	 padding: 10px;
-}
- 
 </style>
